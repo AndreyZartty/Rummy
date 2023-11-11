1134f490382cd4c79876cd51730b6af123e977d7
Andrey <andreysansab@gmail.com>
2023-11-10 19:30:44 -0600

    Creacion Partida

--- src/rummyclient/RummyClient.java
+++ src/rummyclient/RummyClient.java
@@ -15,7 +15,6 @@ public class RummyClient {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("HOLAAA");
     }
     
 }
